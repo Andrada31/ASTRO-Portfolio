543ce7c301df84186aad228ed8956ced0ae9ecc3
Andrada <paraczkiandrada31@gmail.com>
2026-07-16 21:22:44 +0300

    configured astro for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Andrada31.github.io",
+  base: "/ASTRO-Portfolio",
+
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
